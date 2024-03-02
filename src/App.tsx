3078de333f6ac0527451164a20e993cdca080509
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2024-03-02 16:46:54 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url : string | undefined = process.env.REACT_APP_API_URL
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
